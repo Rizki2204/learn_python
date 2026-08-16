990462858162d299f33ed2d01e80f8aac1b48eb4
Rizki2204 <sireden22@gmail.com>
2026-08-17 04:27:50 +0700

    balik ke sebelumnya

--- first_bot.py
+++ first_bot.py
@@ -5,5 +5,3 @@ b = float(input("Masukan angka kedua: "))
 penjumlahan = tambah(a, b)
 print(f"Hasil penjumlahan = {penjumlahan}")
 
-po3ur2930ru328r73r8937r328r7238r73
-fiqehr87erybq8ryq389cr6q38638r7irqe8r9 73287ur3ur839u
