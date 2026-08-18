b4aaca6a1e08c74b7c4d161817ca811cdc0d4d99
Rizki2204 <sireden22@gmail.com>
2026-08-18 14:12:27 +0700

    kode Bot Tele

--- first_bot.py
+++ first_bot.py
@@ -1,7 +1,9 @@
-def tambah(a, b):
-    return a + b
-a = float(input("Masukan angka pertama: "))
-b = float(input("Masukan angka kedua: "))
-penjumlahan = tambah(a, b)
-print(f"Hasil penjumlahan = {penjumlahan}")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Halo! Bot-nya jalan!")
+
+app = ApplicationBuilder().token("8272041378:AAHRbaKhjQsdd1QwGQWhp0_ccYXOiGTLSDU").build()
+app.add_handler(CommandHandler("start", start))
+app.run_polling()
