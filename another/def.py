5d5b67d8470277dc3ec64b6f89981ae8e22209f8
Rizki2204 <sireden22@gmail.com>
2026-08-20 14:02:31 +0700

    membuat folder latihan baru

---
+++ another/def.py
@@ -0,0 +1,7 @@
+def hitung_lpp(x, y):
+        x * y
+        return x * y
+a = float(input("masukan angka pertama: "))
+b = float(input("masukan angka kedua: "))
+hitung_lpp(a, b)
+print(f"hasil = {hitung_lpp}")
--- first_bot.py
+++ first_bot.py
@@ -4,6 +4,10 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Halo! Bot-nya jalan!")
 
+async def about(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hai, dengan siapa disini?")    
+
 app = ApplicationBuilder().token("8272041378:AAHRbaKhjQsdd1QwGQWhp0_ccYXOiGTLSDU").build()
 app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("about", about))
 app.run_polling()
