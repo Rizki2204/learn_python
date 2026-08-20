e791d10d5f6d7e91ca651daece2dbb03ddfdb3e2
Rizki2204 <sireden22@gmail.com>
2026-08-20 15:43:12 +0700

    Menambahkan struktur data

--- another/def.py
+++ another/def.py
@@ -1,7 +1,3 @@
-def hitung_lpp(x, y):
-        x * y
-        return x * y
-a = float(input("masukan angka pertama: "))
-b = float(input("masukan angka kedua: "))
-hitung_lpp(a, b)
-print(f"hasil = {hitung_lpp}")
+def sapa(nama, salam="halo"):
+    print(f"{salam}, {nama}")
+sapa("budi")    
